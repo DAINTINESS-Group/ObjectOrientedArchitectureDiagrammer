6e8273e90f6cdb3299382715ae3fbc27104eaf3b
billdodis <billdodis324@gmail.com>
2023-05-16 03:32:07 +0300

    PlantUML Cropped Image Issue
    Added the reduce of the graph's width, when the extracted image is bigger than 4096x4096.

--- src/main/java/model/diagram/plantuml/PlantUMLExporter.java
+++ src/main/java/model/diagram/plantuml/PlantUMLExporter.java
@@ -16,7 +16,7 @@ public class PlantUMLExporter {
 	
     public PlantUMLExporter() {
     }
-	
+    
     public void exportDiagram(Path selectedFile, String nodesBuffer, String edgesBuffer, boolean packageDiagram) {
     	String plantUMLCode;
     	if (packageDiagram) {
@@ -33,17 +33,50 @@ public class PlantUMLExporter {
     	plantUMLCode += nodesBuffer;
     	plantUMLCode += edgesBuffer;
     	plantUMLCode += "@enduml\n";
-    	System.out.println(plantUMLCode);
+
     	ByteArrayOutputStream png = new ByteArrayOutputStream();
     	try {
-        	SourceStringReader reader = new SourceStringReader(plantUMLCode);
+    		SourceStringReader reader = new SourceStringReader(plantUMLCode);
         	reader.outputImage(png).getDescription();
 			byte [] data = png.toByteArray();
 		    InputStream in = new ByteArrayInputStream(data);
 		    BufferedImage convImg = ImageIO.read(in);
+		    int width = convImg.getWidth();
+            int wrapWidth = 150;
+            //int stringChangerCounter = 0; 
+            if (width == 4096) {
+            	png = new ByteArrayOutputStream();
+            	plantUMLCode = stringChanger(plantUMLCode, wrapWidth);
+            	reader = new SourceStringReader(plantUMLCode);
+            	reader.outputImage(png).getDescription();
+    			data = png.toByteArray();
+    		    in = new ByteArrayInputStream(data);
+    		    convImg = ImageIO.read(in);
+    		    width = convImg.getWidth();
+                //stringChangerCounter ++;
+            }
 		    ImageIO.write(convImg, "png", new File(selectedFile.toString()));
         } catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+    
+    private String stringChanger(String plantCode, int wrapWidth){
+    	String updatedString;
+    	//if (counter == 0) {
+    	int indexOfNewLine = plantCode.indexOf("\n");
+        String firstPart = plantCode.substring(0, indexOfNewLine + 1);
+       	String secondPart = plantCode.substring(indexOfNewLine + 1);
+       	updatedString = firstPart + "skinparam wrapWidth " + wrapWidth + "\n" + secondPart;
+        // !! COMMENTED CODE HERE - KEEP REDUCING THE WRAPWIDTH PARAMETER IN ORDER TO FIT PROPERLY THE IMAGE
+        // DOESNT WORK PROPERLY, COMMENTED JUST TO KEEP THE IDEA. 
+        // POP UP MESSAGE CAN BE ADDED TO INFORM THE USER THAT THE IMAGE HE REQUESTED IS OVER 4096x4096
+        // SO WE REDUCE THE WRAPWIDTH TO REDUCE EXTRACTED IMAGE'S WIDTH. NOW THE USER CAN SEE MORE CLASSES.
+//    	}else {
+//    		String[] lines = plantCode.split("\n");
+//    		lines[1] = "skinparam wrapWidth " + wrapWidth;
+//    		updatedString = String.join("\n", lines);
+//    	}
+    	return updatedString;
+    }
 }
