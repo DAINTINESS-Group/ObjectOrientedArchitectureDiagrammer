a2b98b9a3c177d30cbd1b04f0324340872851a4b
Vassilis Zafeiris <bzafiris@uoi.gr>
2025-10-10 10:21:53 +0300

    Implementation of smart arrow shapes - from lib

---
+++ src/main/java/gr/uoi/diantiness/smartgraph/graphview/arrow/SmartClosedArrow.java
@@ -0,0 +1,18 @@
+package gr.uoi.diantiness.smartgraph.graphview.arrow;
+
+import com.brunomnsilva.smartgraph.graphview.SmartArrow;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+
+public class SmartClosedArrow extends SmartArrow {
+    public SmartClosedArrow(double size) {
+        super(size);
+    }
+
+    public void createArrow(double size) {
+        this.getElements().add(new MoveTo((double) 0.0F, (double) 0.0F));
+        this.getElements().add(new LineTo(-size, size));
+        this.getElements().add(new LineTo(-size, -size));
+        this.getElements().add(new LineTo((double) 0.0F, (double) 0.0F));
+    }
+}
---
+++ src/main/java/gr/uoi/diantiness/smartgraph/graphview/arrow/SmartRhomboidArrow.java
@@ -0,0 +1,19 @@
+package gr.uoi.diantiness.smartgraph.graphview.arrow;
+
+import com.brunomnsilva.smartgraph.graphview.SmartArrow;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+
+public class SmartRhomboidArrow extends SmartArrow {
+    public SmartRhomboidArrow(double size) {
+        super(size);
+    }
+
+    public void createArrow(double size) {
+        this.getElements().add(new MoveTo((double) 0.0F, (double) 0.0F));
+        this.getElements().add(new LineTo((double) -2.0F * size, size));
+        this.getElements().add(new LineTo((double) -4.0F * size, (double) 0.0F));
+        this.getElements().add(new LineTo((double) -2.0F * size, -size));
+        this.getElements().add(new LineTo((double) 0.0F, (double) 0.0F));
+    }
+}
